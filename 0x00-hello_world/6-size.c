5abc1d9265ff5e92a4c788a2415fa2773c591222
Madrinmarison <dmwanthi2@gmail.com>
2022-09-11 12:16:37 -0700

    Check the size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various data types
+ * Return: 0, if the program runs successfully
+ */
+int main()
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char);
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d bytes", sizeof(long int));
+	printf("Size of float: %d bytes", sizeof(float));
+	printf("Size of long long intt: %d bytes", sizeoff(long long int));
+	return (0);
+}
