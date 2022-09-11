a4e10bf8b7cb22b0cb972222d91dd76ca0c78012
Madrinmarison <dmwanthi2@gmail.com>
2022-09-11 11:37:50 -0700

    Print with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *   main - prints the phrase "Programming is like building a multilingual puzzle"
+ *   main - prints a statement
  *  Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
