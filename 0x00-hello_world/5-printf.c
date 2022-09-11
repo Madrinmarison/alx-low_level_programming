97c49d8286dacc503a7e1b7dbaeed0221f07f473
Madrinmarison <dmwanthi2@gmail.com>
2022-09-11 11:48:42 -0700

    print using  function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print a statement
+ * Return: 0, if the function was excecuted successifully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
